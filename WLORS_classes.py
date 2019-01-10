93cd19d5b57f807be41891b19a9a81b6090a88c7
JDO <atharos@vivaldi.net>
2019-01-10 23:18:30 +0000

    Introduced Character and locale objects, storage classes to come

--- WLORS_classes.py
+++ WLORS_classes.py
@@ -1,6 +1,6 @@
 ## Character Classes
 # Object oriented classes and functions derived from the WLORS script
-
+import numpy as np
 
 def get(St, func):
     try:
@@ -17,7 +17,7 @@ def monikerext(address):
     return address[11:]
 
 class Character:
-    def __init__(self, address, race, class_style, attributes, description):
+    def __init__(self, address, race, class_style, attributes, description, library):
         '''Address is the full phrase that someone would use to identify someone,
         ie Mr. Testy The Brave'''
         self.name = nameext(address)
@@ -26,7 +26,16 @@ class Character:
         self.race = race
         self.class_style = class_style
         self.attributes = attributes
-        self.description = description        
+        self.description = description       
+        self.library = library
+    def localise(self, locale):
+        self.locale = locale 
+    def updatelocale(self, newlocale):
+        print(self.locale.scene)
+        newlocale = get( 'where next? ',str)
+        for i in self.library.locales:
+            if i.scene == newlocale:
+                self.locale = i
         
 def character_creation():
     return Character(get('Address', str),
@@ -36,4 +45,60 @@ def character_creation():
                               'Dexterity','Wisdom','Madness','Luck',
                               'Fortitude','Wits']},
                      get('Description',str))
-    
+
+class Locale:
+    def __init__(self, name, open_closed, coord, connectionref):
+        '''
+        I would want to include as an argument to initialiser a connection element
+        which would indicate where it can connect to integrating with its spillover
+        connections
+        '''
+        self.name = name 
+        self.open_closed = open_closed
+        self.coord = coord
+        self.connectionref
+
+
+    def inon(self):
+        '''
+        just describes if the location is enclosed or not
+        ''' 
+        return self.open_closed
+
+    def scene(self):
+        '''
+        Currently just returns the name of the Locale, eventually would generate 
+        a better description
+        '''
+        return 'you are' + str(self.open_closed) + self.name
+
+
+def create_locale():
+    return Locale(get('Locale name: ',str), 
+                  get('open? : ', bool), 
+                  get('co-ordinates? ', np.array), 
+                  get('connection reference', str ))
+# the coordinate argument can be that of a reference value in the spillover matrix.
+# that reference value can also be pointed to in a dictionary with the position of 
+# the locale this would also allow for the rooms to be created, their locations to
+# be embedded in this dictionary, and then for a routine to run through and calculate
+# the connectiveness between locales, which can be at first generated as a function
+# of the terrain in between the two spaces, but then over time become a dynamic 
+# connectiveness determined at least partially by traffic
+
+class Library:
+    def __init__(self, name):
+        '''
+        Defines the library class, which is the current format of the structure of 
+        things known to an entity
+        '''
+        self.name = name
+
+class Environment:
+    def __init__(self, name):
+        '''
+        Defines the Environment class, which contains multiple locales and describes 
+        and calculates their relationships with each other, consequently allowing 
+        movement between them
+        '''
+        self.name = name
